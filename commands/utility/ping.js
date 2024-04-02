1057096bd22164c8dca016c91ef10a793c3ac80a
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2024-04-02 21:49:52 +0900

    Feat Create Slash Command

---
+++ commands/utility/ping.js
@@ -0,0 +1,11 @@
+// 슬래시 명령어 정의
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  async execute(interaction) {
+    await interaction.reply("Pong!");
+  },
+};
